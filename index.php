80f0ce3af5b387ae1abe36e6d436ec6e8966b722
Ahmed ALOUANE <ahmed-taha.alouane@polytechnique.edu>
2018-12-27 21:54:35 +0100

    merged and fixed index.php bizarre situation

--- index.php
+++ index.php
@@ -1,50 +1,33 @@
 <?php
-if (isset($_SESSION["logged_in"])) {
-    // if the user is logged in, redirect him to the dashboard
-    // always call exit() after header() (to avoid php logic to execute after it)
-    header("location: dashboard.php");
-    exit();
-} else {
-    if (!isset($_GET["action"])) {
-        // default landing page is the login page
-        $_GET["action"] = "login";
-    }
-    if ($_GET["action"] == "register") {
-      require("utils/access/registration.php");
-        if (isset($_POST["email"], $_POST["pwd"], $_POST["login"], $_POST["name"], $_POST["surname"], $_POST["birthday"], $_POST["conf"])) {
-            $feedback = Registration::attempt($dbh, $_POST);
-            Registration::generate_form($feedback, $_POST);
+session_name("peanutbutterandjelly");
+session_start();
+require_once("utils/helper.php");
+require_once("utils/database.php");
 
-        } else {
-          Registration::generate_form(Registration::$FEEDBACK["no_feedback"], array());
-        }
-    } else if ($_GET["action"] == "forgot") {
-      require("utils/access/forgot_password.php");
-      if (isset($_POST["email"])){
-        $feedback = ResetPassword::attempt($dbh, $_POST["email"]);
-        ResetPassword::generate_form($feedback, $_POST["email"]);
-      }
-      else{
-        ResetPassword::generate_form(ResetPassword::$FEEDBACK["no_feedback"], "");
-      }
-    } else {
-        // either $_GET["action"] = "login"
-        // or $_GET["action"] is something else that is not "login" or "register" or "forgot",
-        // for which we redirect to login section
-        require("utils/access/login.php");
-        if (isset($_POST["email"], $_POST["pwd"])) {
-          $feedback = Login::attempt($dbh, $_POST["email"], $_POST["pwd"]);
-          if ($feedback["success"]){
-              // successful login
-              $_SESSION["logged_in"] = true;
-              // again, EXIT always after header()
-              header("location: dashboard.php");
-              exit();
-          } else {
-            Login::generate_form($feedback, $_POST["email"]);
-          }
-        } else {
-            Login::generate_form(Login::$FEEDBACK["no_feedback"], array());
-        }
-    }
+if (isset($_GET["page"]) && check_page($_GET["page"], $index_pages)) {
+    $page = $_GET["page"];
+} else {
+    $page = "login";
 }
+
+$dbh = Database::connect();
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="fr-FR">
+ <?php generate_header($page, "css/login.css", $index_pages); ?>
+ <body class="text-center">
+   <!--<div class="container-fluid">-->
+
+     <?php require_once("content/index/content_".$page.".php"); ?>
+
+   <!--</div>-->
+   <script src="js/popper.min.js"></script>
+   <script src="js/jquery.min.js"></script>
+   <script src="js/bootstrap.min.js"></script>
+
+   <?php
+   $dbh = null;
+   ?>
+ </body>
+ </html>
